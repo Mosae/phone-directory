67ba601eebaddd616275aa99968eb607cc24798d
Mosae-Litsoane <mosaelitsoane@gmail.com>
2021-02-22 21:56:00 -0600

    adding rows

---
+++ README.md
@@ -0,0 +1,11 @@
+Contact name - 40 chars limit
+Mobile number - 10 digits
+Email - valid
+
+Add contact button:
+onclick - add info under contacts summary
+These have to be sorted by name
+
+On error: Show invalid input message
+
+I will be using jquery
--- view/js/script.js
+++ view/js/script.js
@@ -1,23 +1,46 @@
-// JS code goes here
-const names = document.getElementById('name');
-const mobile = document.getElementById('mobile');
-const submit = document.getElementById('submit');
-const error = document.getElementById('error');
+function validateForm() {
+	const name = document.getElementById('name').value;
+	const number = document.getElementById('mobile');
+	const email = document.getElementById('email');
+	const error = document.getElementById('error');
 
-submit.addEventListener('click', (e) => {
-	e.preventDefault();
-	//alert(error);
-	// if (typeof names === 'undefined' || names === null || names === '') {
-	// 	alert('please fill in name');
-	// } else {
-	// 	alert('doenst exist');
-	// }
-});
-//check if name exists
-function validateForm(input) {
-	if (input.value.length == 0) {
-		alert('fill in name');
+	if (name.value === '' || number.value === '' || email.value === '') {
+		error.style.display = 'block';
+		// name.focus();
 		return false;
+	} else {
+		addData();
+		// var row = '';
+		// row +=
+		// 	'<tr><td>' +
+		// 	name +
+		// 	'</td><td>' +
+		// 	number +
+		// 	'</td><td>' +
+		// 	email +
+		// 	'</td></tr>';
+		// let tableRef = document.getElementById('list').getElementsByTagName('tbody');
+		// let newRow = tableRef.insertRow(tableRef.row.length)
 	}
-	return true;
+
+	function addData() {
+		//logic to add info
+		let table = document.getElementById('list');
+
+		let rowCount = table.rows.length;
+		let row = table.insertRow(rowCount);
+
+		//DRY code look to make it cleaner
+		let firstCell = row.insertCell();
+		let secondCell = row.insertCell(1);
+		let thirdCell = row.insertCell(2);
+
+		let firstEle = document.createElement('tr');
+		let secondEle = document.createElement('tr');
+		let thirdEle = document.createElement('tr');
+
+		firstCell.appendChild(firstEle);
+		secondCell.appendChild(secondEle);
+		thirdCell.appendChild(thirdEle);
+	}
 }
