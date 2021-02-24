c73062d19aca693dcb0e0f4d200e6d4023603fdb
Mosae-Litsoane <mosaelitsoane@gmail.com>
2021-02-23 23:46:15 -0600

    trying a different funtion to add data

--- view/js/script.js
+++ view/js/script.js
@@ -1,46 +1,54 @@
-function validateForm() {
-	const name = document.getElementById('name').value;
-	const number = document.getElementById('mobile');
-	const email = document.getElementById('email');
-	const error = document.getElementById('error');
+// function validateForm() {
+// 	const name = document.getElementById('name').value;
+// 	const number = document.getElementById('mobile');
+// 	const email = document.getElementById('email');
+// 	const error = document.getElementById('error');
+// 	const submit = document.getElementById('submit');
 
-	if (name.value === '' || number.value === '' || email.value === '') {
-		error.style.display = 'block';
-		// name.focus();
-		return false;
-	} else {
-		addData();
-		// var row = '';
-		// row +=
-		// 	'<tr><td>' +
-		// 	name +
-		// 	'</td><td>' +
-		// 	number +
-		// 	'</td><td>' +
-		// 	email +
-		// 	'</td></tr>';
-		// let tableRef = document.getElementById('list').getElementsByTagName('tbody');
-		// let newRow = tableRef.insertRow(tableRef.row.length)
-	}
+// 	submit.addEventListener('click', displayData);
 
-	function addData() {
-		//logic to add info
-		let table = document.getElementById('list');
+// 	if (name.value === '' || number.value === '' || email.value === '') {
+// 		error.style.display = 'block';
+// 		// name.focus();
+// 		return false;
+// 	} else {
+// 		addData();
+// 	}
 
-		let rowCount = table.rows.length;
-		let row = table.insertRow(rowCount);
+// function addInput(row) {
+// 	var row = '';
+// 	row +=
+// 		'<tr><td>' +
+// 		name +
+// 		'</td><td>' +
+// 		number +
+// 		'</td><td>' +
+// 		email +
+// 		'</td></tr>';
+// 	let tableRef = document
+// 		.getElementById('list')
+// 		.getElementsByTagName('tbody');
+// 	let newRow = tableRef.insertRow(tableRef.row.length);
+// }
 
-		//DRY code look to make it cleaner
-		let firstCell = row.insertCell();
-		let secondCell = row.insertCell(1);
-		let thirdCell = row.insertCell(2);
+// function addData() {
+// 	//logic to add info
+// 	let table = document.getElementById('list');
 
-		let firstEle = document.createElement('tr');
-		let secondEle = document.createElement('tr');
-		let thirdEle = document.createElement('tr');
+// 	let rowCount = table.rows.length;
+// 	let row = table.insertRow(rowCount);
 
-		firstCell.appendChild(firstEle);
-		secondCell.appendChild(secondEle);
-		thirdCell.appendChild(thirdEle);
-	}
-}
+// 	//DRY code look to make it cleaner
+// 	let firstCell = row.insertCell();
+// 	let secondCell = row.insertCell(1);
+// 	let thirdCell = row.insertCell(2);
+
+// 	let firstEle = document.createElement('tr');
+// 	let secondEle = document.createElement('tr');
+// 	let thirdEle = document.createElement('tr');
+
+// 	firstCell.appendChild(firstEle);
+// 	secondCell.appendChild(secondEle);
+// 	thirdCell.appendChild(thirdEle);
+// }
+// }
